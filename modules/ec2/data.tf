217b31d5f9494946a24f66f56295486b1bf0b6df
Your Name <you@example.com>
2024-12-02 07:10:33 +0530

    add

--- modules/ec2/data.tf
+++ modules/ec2/data.tf
@@ -4,3 +4,7 @@ data "aws_ami" "ami" {
   name_regex       = "RHEL-9-DevOps-Practice"
   owners           = ["973714476881"]
 }
+
+data "vault_generic_secret" "ssh" {
+  path = "infra-secrets/ssh"
+}
